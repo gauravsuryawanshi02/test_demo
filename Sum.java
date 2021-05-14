4ae0071c067dc7b39fd87e477843129f87f052ed
Your Name <you@example.com>
2021-05-14 16:30:23 +0530

    sum java code

---
+++ Sum.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class Sum{
+	public static void main(String args[]){
+		int sum=0;
+		int i,n,arr[];
+		Scanner s=new Scanner(System.in);
+		System.out.println("ENTER THE NO.OF ARRAY");
+		n=s.nextInt();
+		arr=new int[n];
+		System.out.println("enter those  "+n+"  elements");
+		for(i=0;i<n;i++){
+		arr[i]=s.nextInt();
+}
+		for(i=0;i<n;i++){
+		sum=sum+arr[i];
+
+}
+                System.out.println("sum of array is "+sum);
+}
+}
